96889e20d27579955df532e15da29e419917e29a
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2020-07-20 20:43:05 +1000

    fix: innerHTML

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 const URL = "https://raw.githubusercontent.com/nfcnepal/nfcnepaldata/master/test.json"
 fetch(URL)
-  .then(response => response.json())
-  .then(data => {
+  .then((response) => response.json())
+  .then((data) => {
     gwEle = document.getElementById("gw")
     pointsEle = document.getElementById("points")
-    gwEle.innerHTML(data['Gameweek'])
-    pointsEle.innerHTML(data['Points')
+    gwEle.innerHTML = data['Gameweek']
+    pointsEle.innerHTML = data['Points']
   })
-  .catch(error => console.error(error))
+  .catch((error) => console.error(error))
